e49d8f0af5e2f1adb632f85825b4d40bc73c242c
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-20 23:55:27 +0530

    JavaScript

---
+++ Loops and Strings/loops.js
@@ -0,0 +1,15 @@
+// Loops
+console.log("Loops");
+
+// for Loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Hello!", i);
+}
+
+// calculate sum of of 1 to 5
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum = sum + i;
+}
+console.log("total sum = ", sum);
+
