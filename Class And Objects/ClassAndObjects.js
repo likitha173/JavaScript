d9b28ba75833ec581261f24ac777af50370e7c36
Likitha Khatri <likithakhatri5774@gmail.com>
2023-12-19 01:08:06 +0530

    classes & objects

--- Class And Objects/ClassAndObjects.js
+++ Class And Objects/ClassAndObjects.js
@@ -49,3 +49,101 @@ console.log(fortuner.start())
 let lexus = new ToyotaCar("lexus", 12);
 console.log(lexus)
 console.log(lexus.start())
+
+
+class Parent {
+    hello() {
+        console.log("hello");
+    }
+}
+
+class Child extends Parent {}
+
+let obj = new Child();
+console.log(obj);
+console.log(obj.hello());
+
+
+
+class Person {
+    constructor() {
+        this.species = "homo sapiens"
+    }
+
+    eat() {
+        console.log("eat");
+    }
+    sleep() {
+        console.log("sleep");
+    }
+}
+
+class Engineer extends Person {
+    constructor(branch) {
+        super();
+        this.branch = branch;
+    }
+    works() {
+        console.log("solve problem, build something");
+    }
+}
+
+let likitha = new Engineer("comouter engineer");
+console.log(likitha);
+console.log(likitha.eat());
+
+
+// Q1
+let data = "secret information";
+
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    viewData() {
+        console.log("data: ", data);
+    }
+}
+
+let student1 = new User("likitha", "likitha@gmail.com");
+let student2 = new User("nikitha", "nikitha@gmail.com");
+
+console.log(student1);
+console.log(student1.viewData());
+
+
+// Q2
+class Admin extends User {
+    constructor(name, email) {
+        super(name, email);
+    }
+    editData() {
+        data = "data is edited"
+    }
+}
+
+let admin1 = new Admin("admin", "admin@gmail.com");
+
+console.log(admin1);
+console.log(data);
+console.log(admin1.editData());
+console.log(data);
+
+let a = 5;
+let b = 10;
+
+console.log("a: ", a);
+console.log("b: ", b);
+console.log("a + b: ", a + b);
+console.log("a + b: ", a + b);
+console.log("a + b: ", a + b);
+try {
+    console.log("a + b: ", a + c);
+} catch(err) {
+    console.log(err);
+}
+console.log("a + b: ", a + b);
+console.log("a + b: ", a + b);
+console.log("a + b: ", a + b);
